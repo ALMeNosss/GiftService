98fc2f61ee1f94d95272d6d01aa7330b478eb767
socialprogram <94844457+socialprogram@users.noreply.github.com>
2021-12-27 23:03:56 +0300

    Update models.py

--- core/models.py
+++ core/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class UserModel (models.Model):
+id: models.PositiveIntegerField(unique=True, primary_key=True)
+user_name: models.CharField(max_length=50, unique=True)
+name: models.CharField(max_length=50)
+password: models.CharField(max_length=30) 
+
+class ProductHistoryModel
+id: models.PositiveIntegerField(unique=True, primary_key=True)
+userid=UserModel.id
+count: models.PositiveBigIntegerField()
